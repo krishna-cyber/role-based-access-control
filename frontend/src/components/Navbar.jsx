c1ab1272e4aa0ce61ace621ce10b2b129a85c5c4
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-10-11 19:30:01 +0545

    Navbar component changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
       <nav className=' max-w-4xl mx-auto flex justify-between py-6'>
         <h2 className=' font-medium text-xl'>RBAC</h2>
         <ul className='flex justify-around gap-5 text-orange-600 font-medium'>
-          <li className=' cursor-pointer'>Register</li>
           <li className=' cursor-pointer'>Home</li>
+          <li className=' cursor-pointer'>Register</li>
           <li className=' cursor-pointer'>Login</li>
         </ul>
       </nav>
