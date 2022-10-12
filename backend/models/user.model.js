0c2063a7f401a4b5aae3c9e92cc4f0af24aab45d
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-10-12 20:24:25 +0545

    bcrypt used for hashing passwords

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -18,6 +19,18 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+//hashing password before saving to database
+
+userSchema.pre("save", async function (next) {
+  console.log("just before saving");
+
+  const rounds = 10; // What you want number for round paasword
+
+  const hash = await bcrypt.hash(this.password, rounds);
+  this.password = hash;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
