89ed63da8047bb6ee9e41657bfe3588be635cc61
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-10-12 15:16:15 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 require("./models/dbconnection"); //Database connection file
 
+//Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(routes);
+//error handling middleware
 
 app.all("*", (req, res) => {
   res.json(createError(454, "Page not found"));
